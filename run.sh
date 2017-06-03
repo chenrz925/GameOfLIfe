6651940e82212460398173f1e58b93923f7a93ef
chenrz925 <chenrz925@live.cn>
2017-06-03 17:53:31 +0800

    Update

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 cd out/artifacts/GameOfLIfe_jar/
-java -cp GameOfLIfe.jar GameOfLife.Main
+echo ''
+java -cp GameOfLIfe.jar GameOfLife.Main $1
--- src/GameOfLife/Main.java
+++ src/GameOfLife/Main.java
@@ -8,10 +8,33 @@ package GameOfLife;
  */
 public class Main {
     public static void main(String[] args) {
-        Map map = new Map(80);
+        int size = 80;
+        if (args.length > 0) {
+            switch (Integer.parseInt(args[0])) {
+                case 1:
+                    size = 10;
+                    break;
+                case 2:
+                    size = 20;
+                    break;
+                case 3:
+                    size = 40;
+                    break;
+                case 4:
+                    size = 80;
+                    break;
+                case 5:
+                    size = 160;
+                    break;
+                default:
+                    break;
+            }
+        }
+        Map map = new Map(size);
         Display display = new Display(map);
         Control control = new Control(map);
         display.setVisible(true);
         control.setVisible(true);
+        System.out.println("Map size: " + size);
     }
 }
