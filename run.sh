5b619756181d4df5865fcef3ee1d3dbf7ebf6b6b
chenrz925 <chenrz925@live.cn>
2017-06-03 17:55:32 +0800

    Version 1.10

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 cd out/artifacts/GameOfLIfe_jar/
-echo ''
+echo '1: Map size = 10'
+echo '2: Map size = 20'
+echo '3: Map size = 40'
+echo '4: Map size = 80'
+echo '5: Map size = 160'
 java -cp GameOfLIfe.jar GameOfLife.Main $1
